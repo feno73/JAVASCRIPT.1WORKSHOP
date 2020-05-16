88153ed773273c5f2c0339bf4d530e6e77430ecd
feno73 <diego.el.feno@gmail.com>
2020-05-16 12:21:38 -0300

    Constructor

--- docs/noticias.class.js
+++ docs/noticias.class.js
@@ -8,17 +8,17 @@ class Noticias{
     }
 
     Mostrar(){
-        cconst ficha = doument.createElement("article")
+        const ficha = doument.createElement("article")
         ficha.classList.add("row noticia")
-        ficha.innerHTML =   <div class="col-12 col-md-3 col-lg-4 noticia-img py-2">
-                                <img src="img/sorteo-libertadores.jpg" class="img-fluid" alt="">
-                            </div>
+        ficha.innerHTML =   `<div class="col-12 col-md-3 col-lg-4 noticia-img py-2">
+                                <img src=${this.imagen} class="img-fluid" alt="">
+                             </div>
 
                             <div class="col-12 col-md-9 col-lg-8 noticia-txt py-2">
-                                <h3>La tecnologia llega a la actual edicion de La Libertadores</h3>
-                                <p>por Joaquin Angeli</p>
-                                <p> La Conmebol anunció la implementación del sistema VAR (asistencia arbitral por video) desde las semifinales de la actual Libertadores. </p>
-                            </div>
+                                <h3>${this.titulo}</h3>
+                                <p>por ${this.autor}</p>
+                                <p>${this.detalle}</p>
+                            </div>`
     }
 
 }
